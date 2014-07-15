3a6b7e62c1a2f010d8217801a7974472e33edbc0
Christof Sirk <christof.sirk@gmail.com>
2014-07-15 15:16:35 +0200

    Fix specs; irgnoring dagre-d3 for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,7 @@ module.exports = function(grunt) {
             './bower_components/d3/d3.min.js',
             './vendor/angular-foundation-colorpicker/js/foundation-colorpicker-module.js',
             './vendor/mm-foundation/mm-foundation-tpls-0.1.0.min.js',
-            './vendor/dagre-d3/dagre-d3.min.js',
+            // './vendor/dagre-d3/dagre-d3.min.js',
             // Some source files we'll need to include manually, otherwise
             // the load order is wrong
             'app/js/*.js',
@@ -131,6 +131,7 @@ module.exports = function(grunt) {
             'app/js/util/**/*.js',
             specFiles
           ],
+          exclude: ['app/config.js'],
           frameworks: ['jasmine'],
           browsers : ['PhantomJS'],
           plugins : [
