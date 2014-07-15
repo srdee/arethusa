1f4dc720de2a582226bcb7ea837d6eab98ea06c4
Christof Sirk <christof.sirk@gmail.com>
2014-07-15 11:54:22 +0200

    Add create-final-js task which uses main_template.js

---
+++ app/main_template.js
@@ -0,0 +1,15 @@
+define([ "jquery",
+  "angular",
+  "x2js",
+  "d3",
+  "dagreD3",
+  "UserVoice",
+  "colorpicker.module",
+  "mm-foundation",
+  "angular-route", "angular-resource", "angular-cookies", "angular-scroll"],
+  function($, angular, x2js, d3, dagreD3, UserVoice) {
+    $(document).ready(function() {
+      {{text}}
+      angular.bootstrap(document, ['arethusa']);
+    });
+  });
