3910ce9d6dad9e862d405cb749fc12abba2224b9
Christof Sirk <christof.sirk@gmail.com>
2014-07-16 09:42:20 +0200

    Add CDNs :shipit:

--- app/config.js
+++ app/config.js
@@ -22,6 +22,12 @@ require.config({
     'angular-scroll': {
       deps: ['angular']
     },
+    'angulartics': {
+      deps: ['angular']
+    },
+    'angulartics-ga': {
+      deps: ['angulartics']
+    },
     'mm-foundation': {
       deps: ['angular'],
     },
@@ -40,27 +46,53 @@ require.config({
     }
   },
   paths: {
-    angular: "../bower_components/angular/angular",
-    "angular-animate": "../bower_components/angular-animate/angular-animate",
-    "angular-cookies": "../bower_components/angular-cookies/angular-cookies",
-    "angular-mocks": "../bower_components/angular-mocks/angular-mocks",
-    "angular-resource": "../bower_components/angular-resource/angular-resource",
-    "angular-route": "../bower_components/angular-route/angular-route",
-    "angular-scroll": "../bower_components/angular-scroll/angular-scroll.min",
-    "angulartics": "bower_components/angulartics/dist/angulartics.min",
-    "angulartics-ga": "bower_components/angulartics/dist/angulartics-ga.min",
-    d3: "../bower_components/d3/d3",
-    foundation: "../bower_components/foundation/js/foundation",
-    jquery: "../bower_components/jquery/dist/jquery",
+    angular: [
+      "//ajax.googleapis.com/ajax/libs/angularjs/1.2.17/angular.min",
+      "../bower_components/angular/angular"
+    ],
+    "angular-animate": [
+      "//ajax.googleapis.com/ajax/libs/angularjs/1.2.17/angular-animate.min",
+      "../bower_components/angular-animate/angular-animate"
+    ],
+    "angular-cookies": [
+      "//ajax.googleapis.com/ajax/libs/angularjs/1.2.17/angular-cookies.min",
+      "../bower_components/angular-cookies/angular-cookies"
+    ],
+    "angular-mocks": [
+      "//ajax.googleapis.com/ajax/libs/angularjs/1.2.17/angular-mocks.min",
+      "../bower_components/angular-mocksanimate/angular-mocks"
+    ],
+    "angular-resource": [
+      "//ajax.googleapis.com/ajax/libs/angularjs/1.2.17/angular-resource.min",
+      "../bower_components/angular-resource/angular-resource"
+    ],
+    "angular-route": [
+      "//ajax.googleapis.com/ajax/libs/angularjs/1.2.17/angular-route.min",
+      "../bower_components/angular-route/angular-route"
+    ],
+    "angular-scroll": [
+      "../bower_components/angular-scroll/angular-scroll.min"
+    ],
+    "angulartics": "../bower_components/angulartics/dist/angulartics.min",
+    "angulartics-ga": "../bower_components/angulartics/dist/angulartics-ga.min",
+    d3: [
+      "//cdnjs.cloudflare.com/ajax/libs/d3/3.4.6/d3.min",
+      "../bower_components/d3/d3",
+    ],
+    foundation: [
+      "//cdnjs.cloudflare.com/ajax/libs/foundation/5.2.2/js/foundation.min",
+      "../bower_components/foundation/js/foundation",
+    ],
+    jquery: [
+      "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min",
+      "../bower_components/jquery/dist/jquery",
+    ],
     x2js: "../bower_components/x2js/xml2json.min",
     "mm-foundation": "../vendor/mm-foundation/mm-foundation-tpls-0.2.2",
     "colorpicker.module": "../vendor/angular-foundation-colorpicker/js/foundation-colorpicker-module.min",
     "dagreD3": "../vendor/dagre-d3/dagre-d3.min",
     "UserVoice": "../vendor/uservoice/uservoice.min"
   },
-  packages: [
-
-  ]
 });
 
 requirejs(['../dist/arethusa.min']);
