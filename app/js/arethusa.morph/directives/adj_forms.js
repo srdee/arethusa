f31dc3631066a41b7d89090fbc598758263f00be
LFDM <1986gh@gmail.com>
2014-08-13 21:14:58 +0200

    Add adjective forms

---
+++ app/js/arethusa.morph/directives/adj_forms.js
@@ -0,0 +1,33 @@
+"use strict";
+
+angular.module('arethusa.morph').directive('adjForms', [
+  function() {
+    return {
+      restrict: 'A',
+      scope: {
+        forms: '=adjForms'
+      },
+      link: function(scope, element, attrs) {
+        scope.adverb = scope.forms.pop();
+        scope.principalParts = {};
+
+        scope.tableData = { 1: {}, 2: {} };
+        for (var i=1; i < 7; i++) {
+          scope.tableData[1][i] = { m: [], f: [], n: [] };
+          scope.tableData[2][i] = { m: [], f: [], n: [] };
+        }
+
+        scope.formsAsString = function(forms) {
+          return arethusaUtil.map(forms, function(el) {
+            return el.string;
+          }).join(', ');
+        };
+
+        angular.forEach(scope.forms, function(form, i) {
+          scope.tableData[form.numerus][form.casus][form.sexus].push(form);
+        });
+      },
+      templateUrl: 'templates/arethusa.morph/adj_forms.html'
+    };
+  }
+]);
